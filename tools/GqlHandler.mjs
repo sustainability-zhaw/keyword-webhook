064abe4723f91f07a62f35ef8a311a93c130bdb8
Christian Glahn <glah@zhaw.ch>
2023-02-13 15:58:07 +0100

    drop stale mapping shortcuts

--- tools/GqlHandler.mjs
+++ tools/GqlHandler.mjs
@@ -32,7 +32,20 @@ export async function cleanup_selective(target, regex) {
         msg
     }`).join("");
 
-    const query = `mutation { ${dropper} }`;
+    const clearSdg = `mutation {
+        updateInfoObject(input: {
+          filter: { has: sdgs },
+            remove: {
+            sdgs: [{ id: "sdg_${regex.replace("sdg", "")}" }]
+          }
+        } ) {
+          infoObject {
+            link
+          }
+        }
+      }`;
+
+    const query = `mutation { ${dropper} ${clearSdg} }`;
       
     const result = await runRequest(target, { query });
     
